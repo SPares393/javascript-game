1186c945aa4a62ddc903ab91abdd662b2c802c29
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-08-03 13:36:52 +0100

    start game function added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const gameBoard = document.querySelector(".game-board");
+const gameStart = document.querySelector(".game-start");
 const dice = document.querySelector(".dice");
+const gamePieceOne = `<i class="fas fa-user game-piece-1"></i>`;
+const gamePieceTwo = `<i class="fas fa-user game-piece-2"></i>`;
+
+let isPlaying = false;
 
 // CREATE GAME BOARD
 const makeGrid = function (rows, cols) {
@@ -9,10 +14,20 @@ const makeGrid = function (rows, cols) {
 };
 makeGrid(10, 10);
 
+// INITIATE GAME
+gameStart.addEventListener("click", function () {
+  isPlaying = true;
+  document.querySelector(
+    ".square-1"
+  ).innerHTML += `${gamePieceOne} ${gamePieceTwo}`;
+});
+
 // DICE ROLL FUNCTIONALITY
 dice.addEventListener("click", function () {
-  const diceRoll = Math.trunc(Math.random() * 6) + 1;
-  // DISPLAY DICE ROLL
-  // dice.innerHTML = `<img src="/images/dice-${dice}.png" alt="game_dice" class="dice" />`;
-  dice.src = `/images/dice-${diceRoll}.png`;
+  if (isPlaying === true) {
+    const diceRoll = Math.trunc(Math.random() * 6) + 1;
+    // DISPLAY DICE ROLL
+    // dice.innerHTML = `<img src="/images/dice-${dice}.png" alt="game_dice" class="dice" />`;
+    dice.src = `/images/dice-${diceRoll}.png`;
+  }
 });
