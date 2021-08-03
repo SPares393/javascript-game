4577103530e4800d5db9a8bff408881b343145c1
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-08-03 13:09:21 +0100

    dice roll generated

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const gameBoard = document.querySelector(".game-board");
+const dice = document.querySelector(".dice");
 
+// CREATE GAME BOARD
 const makeGrid = function (rows, cols) {
   for (i = rows * cols; i > 0; i--) {
     gameBoard.innerHTML += `<div class="game-board-square square-${i}">${i}</div>`;
   }
 };
 makeGrid(10, 10);
+
+// DICE ROLL FUNCTIONALITY
+dice.addEventListener("click", function () {
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  // DISPLAY DICE ROLL
+  // dice.innerHTML = `<img src="/images/dice-${dice}.png" alt="game_dice" class="dice" />`;
+  dice.src = `/images/dice-${diceRoll}.png`;
+});
