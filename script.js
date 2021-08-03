f4ab52d79515efba4857f58cac507fa23ef995fc
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-08-03 12:48:39 +0100

    basic game layout

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const gameBoard = document.querySelector(".game-board");
 
+const makeGrid = function (rows, cols) {
+  for (i = rows * cols; i > 0; i--) {
+    gameBoard.innerHTML += `<div class="game-board-square square-${i}">${i}</div>`;
+  }
+};
+makeGrid(10, 10);
